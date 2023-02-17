15a14efb7dd8ffe53b4ba1adc9682e0da957c3b5
Max Dekel <max@dekelnet.com>
2023-02-17 17:17:23 -0500

    new scraper + more text

--- scraper/cleanup.py
+++ scraper/cleanup.py
@@ -1,33 +1,30 @@
-#program to look at downloaded text files and remove any that are empty or have less than 50 words
 import os
 
 
+# Function to remove all files in a directory wth less than 10 words
+def cleanup(path):
+    files = os.listdir(path)
+    for file in files:
+        with open(path + file) as f:
+            words = f.read().split()
+            if len(words) < 10:
+                os.remove(path + file)
+                print("removed " + file)
+
+# Function to remove all duplicate files in a directory using diff and rm
+def remove_duplicates(path):
+    files = os.listdir(path)
+    for file in files:
+        for file2 in files:
+            if file != file2:
+                if os.system("diff " + path + file + " " + path + file2 + " > /dev/null") == 0:
+                    os.remove(path + file2)
+                    print("removed " + file2)
+
+
+
 path = "/home/player1/Desktop/RPI/ChatRPI/text/"
-def main():
-    files = os.listdir(path)
-    for file in files:
-        file = path + file
-        with open(file, 'r') as f:
-            lines = f.readlines()
-            if len(lines) == 0:
-                os.remove(file)
-                continue
-            words = 0
-            for line in lines:
-                words += len(line.split())
-            if words < 100:
-                os.remove(file)
-
-#funtion to remove all special characters froms in a directory
-def remove_special_characters(path):
-    files = os.listdir(path)
-    for file in files:
-        file = path + file
-        with open(file, 'r') as f:
-            lines = f.readlines()
-        with open(file, 'w') as
-
-
 
 if __name__ == "__main__":
-    main()
+    remove_duplicates(path)
+    cleanup(path)
--- scraper/main.py
+++ scraper/main.py
@@ -2,14 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 
-#function to get all p tags from a url
+#function to get all text from a url
 def get_p_tags(url):
     try:
         r = requests.get(url)
     except:
         return []
     soup = BeautifulSoup(r.text, 'html.parser')
-    return soup.find_all('p')
+    text = (soup.find_all('p') + 
+            soup.find_all('li') + 
+            soup.find_all('hr') +
+            soup.find_all('div'))
+    return text
 
 #function to get all urls from a url. make sure they contain rpi.eu
 def get_urls(url):
@@ -39,13 +43,16 @@ url = "https://www.rpi.edu/"
 url2 = "http://catalog.rpi.edu/content.php?catoid=24&navoid=606"
 urls = [url, url2]
 visited = []
+counter = 0
 while (len(urls) != 0):
     if url in visited:
+        print("already visited")
         url = urls.pop()
         continue
     visited.append(url)
     url = urls.pop()
     p_tags = get_p_tags(url)
-    parsed_url = url.replace("https://", "").replace("http://", "").replace("/", "_")
-    write_to_file(p_tags, path + parsed_url + ".txt")
+    print(url)
+    #write_to_file(p_tags, path + str(counter) + ".txt")
     urls += get_urls(url)
+    counter += 1
