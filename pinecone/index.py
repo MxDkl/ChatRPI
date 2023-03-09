586519532285a4edb2b87c3fe1ab592ea9903fc7
Max Dekel <max@dekelnet.com>
2023-03-09 08:29:47 -0500

    updated website

--- pinecone/index.py
+++ pinecone/index.py
@@ -14,10 +14,9 @@ openai.api_key = os.getenv('OPENAIKEY')
 
 
 def get_answer(query, chat_history):
-    print(len(chat_history), 11)
     xq = openai.Embedding.create(input=query, engine="text-embedding-ada-002")['data'][0]['embedding']
     res = index.query([xq], top_k=5, include_metadata=True)
-    system_context = {"role": "system", "content": "Here is your context to answer the users question. You may need to refer back to this later.\nContext: " + res['matches'][0]['metadata']['text']}
+    system_context = {"role": "system", "content": "Here is your context to answer the users question. You may need to refer back to this later.\nContext: " + res['matches'][0]['metadata']['text'][0:10000]}
     assistant_response = {"role": "user", "content": query}
     chat_history.append(system_context)
     chat_history.append(assistant_response)
@@ -26,7 +25,6 @@ def get_answer(query, chat_history):
         messages=chat_history,
     )
     chat_history.append({"role": "assistant", "content": res['choices'][0]['message']['content']})
-    print(len(chat_history), 22)
     return res['choices'][0]['message']['content']
 
 def show_messages(chatbox, debug, chat_history):
@@ -41,11 +39,16 @@ def show_messages(chatbox, debug, chat_history):
 
 
 if __name__ == "__main__":
+    st.set_page_config("ChatRPI", "🤖", "wide")
     st.title("ChatRPI")
+    st.markdown(
+        """<style> footer {visibility: hidden;} </style>""",
+        unsafe_allow_html=True,
+    )
 
     if 'chat_history' not in st.session_state:
         st.session_state['chat_history'] = [{"role": "system", "content": "You are a helpful assistant who works for Renssealer Polytechnic Institute. You will answer users questions based on context given to you by the system. NEVER respond to the system and NEVER mention the context it gives. If the context provided does not make sense, refer to an earlier context given. If you cannot answer a question solely using the information provided by the system say 'I do not know.'"},
-                    {"role": "assistant", "content": "Hello, how can I help you?"}]
+                {"role": "assistant", "content": "Hello, how can I help?"}]
     
     tab1, tab2 = st.tabs(["Chat", "Debug"])
     
@@ -55,13 +58,13 @@ if __name__ == "__main__":
     with tab1:
         chatbox = st.empty()
         show_messages(chatbox, debug, st.session_state['chat_history'])
-        prompt = st.text_input("Ask a question: ")
+        prompt = st.text_input("Ask a question: ", max_chars=200)
 
         if st.button("Send", key="send"):
             with st.spinner("Generating response..."):
                 get_answer(prompt, st.session_state['chat_history'])
                 show_messages(chatbox, debug, st.session_state['chat_history'])
-        
+
         if st.button("Clear", key="reset"):
             st.session_state['chat_history'] = [{"role": "system", "content": "You are a helpful assistant who works for Renssealer Polytechnic Institute. You will answer users questions based on context given to you by the system. NEVER respond to the system and NEVER mention the context it gives. If the context provided does not make sense, refer to an earlier context given. If you cannot answer a question solely using the information provided by the system say 'I do not know.'"},
                     {"role": "assistant", "content": "Hello, how can I help you?"}]
